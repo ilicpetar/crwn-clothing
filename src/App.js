6cbc1add2f1f2af7859016315b2b68115b2bd088
ilic.petar <ilic.petar@gmail.com>
2020-05-09 17:26:12 +0200

    build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
-  return(
-  <div>
-    <HomePage />
-  </div>
-  )
-
+  return (
+    <div>
+      <Switch>
+        <Route exact to="/" component={HomePage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
