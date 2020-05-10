340ec3b4d5d5673eaa3e5674e993ceede7bff127
ilic.petar <ilic.petar@gmail.com>
2020-05-10 15:45:54 +0200

    Updates

---
+++ precache-manifest.d684e7bfd216b9b5be5e82280ce8117b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8ee88748e6c7574cf6f71ff0906f992",
+    "url": "/crwn-clothing/index.html"
+  },
+  {
+    "revision": "05d23f2934b39f9c4857",
+    "url": "/crwn-clothing/static/css/main.613b697b.chunk.css"
+  },
+  {
+    "revision": "fce0ecdea171513adec3",
+    "url": "/crwn-clothing/static/js/2.cd07de49.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/crwn-clothing/static/js/2.cd07de49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d23f2934b39f9c4857",
+    "url": "/crwn-clothing/static/js/main.207d6663.chunk.js"
+  },
+  {
+    "revision": "0e46efea361dabb92088",
+    "url": "/crwn-clothing/static/js/runtime-main.4e42e579.js"
+  }
+]);
