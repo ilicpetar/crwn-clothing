22e23f99914cc1021ce24a82066f7e45dd13172e
ilic.petar <ilic.petar@gmail.com>
2020-05-21 20:05:01 +0200

    Adding shop page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component.jsx";
+
 import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact to="/" component={HomePage} />
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
